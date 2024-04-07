42a183a789be89ebe3d42ac1963e136ac86cd2e3
kritikakit <147742617+kritikakit@users.noreply.github.com>
2024-04-07 18:54:22 -0400

    Update Save.php

--- Save.php
+++ Save.php
@@ -11,14 +11,12 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Check if image file is selected
 if(isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
     $image_name = $_FILES['image']['name'];
     $image_tmp_name = $_FILES['image']['tmp_name'];
     $image_size = $_FILES['image']['size'];
     $image_type = $_FILES['image']['type'];
     
-    // Move uploaded image to a designated folder
     $upload_dir = "advertize";
     $target_file = $upload_dir . basename($image_name);
     move_uploaded_file($image_tmp_name, $target_file);
