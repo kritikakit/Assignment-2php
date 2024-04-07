231e6e4dc4780a49f89d031bff1571087153bb97
kritikakit <147742617+kritikakit@users.noreply.github.com>
2024-04-07 18:49:13 -0400

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Replace with your authentication logic
     if ($username === "Kriti" && $password === "1234") {
         $_SESSION['loggedin'] = true;
         header("Location: AddProduct.html");
