14506a9a7bfe3362c50b3fff8f0180d8f9190255
kritikakit <147742617+kritikakit@users.noreply.github.com>
2024-04-07 18:48:14 -0400

    Update View.php

--- View.php
+++ View.php
@@ -90,7 +90,7 @@ $conn->close();
 </div>
 <footer> <!-- Footer section -->
         <div class="container">
-            <p>&copy; 2024 Vaseline Corporation</p> <!-- Copyright information -->
+            <p>&copy; 2024 Vaseline Corporation</p> 
         </div>
     </footer>
 
